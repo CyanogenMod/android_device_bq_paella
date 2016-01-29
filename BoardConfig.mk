d1d85d58569ff185ef431dbf7834bdedbd96ee80
Francisco Javier Martín Otero <francisco.martin@bq.com>
2016-01-29 13:19:18 +0100

    Add Camera
    Change-Id: Iff196f8f4ffceec3c97e22fb417d4c76e6423642

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,9 +36,17 @@ CONFIG_EAP_PROXY := qmi
 CONFIG_EAP_PROXY_DUAL_SIM := true
 
 # Camera
--include $(DEVICE_PATH)/camera/CameraConfig.mk
+BOARD_CAMERA_SENSORS := \
+    imx214_olqba22 \
+    imx214_f13n05k \
+    s5k5e2_olq5f20 \
+    s5k5e2_s7b5
+
+TARGET_USE_VENDOR_CAMERA_EXT := true
+USE_DEVICE_SPECIFIC_CAMERA := true
 
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+TARGET_CAMERA_HAL_PATH := $(DEVICE_PATH)/camera
 
 #Audio
 BOARD_USES_GENERIC_AUDIO := true
