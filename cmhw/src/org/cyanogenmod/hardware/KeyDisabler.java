dc76c57bf9241aa5145ec97c75ca5a6ab5d1e437
Marcos Marado <mmarado@cyngn.com>
2016-04-01 15:30:08 +0100

    KeyDisabler: don't assume CONTROL_PATH exists

--- cmhw/src/org/cyanogenmod/hardware/KeyDisabler.java
+++ cmhw/src/org/cyanogenmod/hardware/KeyDisabler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package org.cyanogenmod.hardware;
 
+import java.io.File;
 import org.cyanogenmod.hardware.util.FileUtils;
 
 /*
@@ -31,14 +32,18 @@ public class KeyDisabler {
 
     private static String CONTROL_PATH = "/sys/android_touch/buttons";
 
-    public static boolean isSupported() { return true; }
+    public static boolean isSupported() {
+        return new File(CONTROL_PATH).exists();
+    }
 
     public static boolean isActive() {
-        return (FileUtils.readOneLine(CONTROL_PATH).equals("0"));
+        if (isSupported()) return (FileUtils.readOneLine(CONTROL_PATH).equals("0"));
+        return false;
     }
 
     public static boolean setActive(boolean state) {
-        return FileUtils.writeLine(CONTROL_PATH, (state ? "0" : "1"));
+        if (isSupported()) return FileUtils.writeLine(CONTROL_PATH, (state ? "0" : "1"));
+        return false;
     }
 
 }
