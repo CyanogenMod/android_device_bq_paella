7dfff63bf784b8683367283a9410070940f38e2c
Marcos Marado <mmarado@cyngn.com>
2016-06-06 13:55:53 +0000

    Allow IPv6 tethering
    Issue: PAELLA-86
    Change-Id: I65df72d3c2d74c67e722e251964666ea1e9bfb37
    (cherry picked from commit b74a6486b83503116f5954d06cd1bcd6671b4fc0)

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -110,7 +110,8 @@ PRODUCT_PACKAGES += \\
 PRODUCT_PACKAGES += \\
     com.qti.dpmframework \\
     dpmapi \\
-    ConnectivityExt
+    ConnectivityExt \\
+    QtiTetherService
 
 PRODUCT_PACKAGES += \\
     qcnvitems \\
@@ -202,6 +203,16 @@ include \$(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
 LOCAL_MODULE_OWNER := $VENDOR
 LOCAL_SRC_FILES := proprietary/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := QtiTetherService
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/QtiTetherService/QtiTetherService.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
