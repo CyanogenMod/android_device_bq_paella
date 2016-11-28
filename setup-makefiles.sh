9cc61b128bab7fd960135885f6e4867a21783d46
Marcos Marado <mindboosternoori@gmail.com>
2016-11-28 14:38:29 +0000

    enable QtiTelephonyService
    ...and, with it, enable SIM toggles.
    
    Change-Id: If30433cab408241063d345af06efd38598ffedf7

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -111,7 +111,8 @@ PRODUCT_PACKAGES += \\
     com.qti.dpmframework \\
     dpmapi \\
     ConnectivityExt \\
-    QtiTetherService
+    QtiTetherService \\
+    QtiTelephonyService
 
 PRODUCT_PACKAGES += \\
     qcnvitems \\
@@ -220,6 +221,17 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := QtiTelephonyService
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/app/QtiTelephonyService/QtiTelephonyService.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
